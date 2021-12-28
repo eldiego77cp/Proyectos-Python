5d9e04890af5a98fb9d636fb5ab0b0b02dc897e3
Alejandro Fernandez <88398189+eldiego77cp@users.noreply.github.com>
2021-12-28 18:27:18 -0300

    Update 20122021 18:27

---
+++ libraries.py
@@ -0,0 +1,51 @@
+# Librerías o módulos en Python
+# 
+# En primero lugar, para acceder al help de los módulos es necesario importarlos para luego pedir las referencias del módulo según los siguientes comandos a modo de ejemplo
+
+import os
+help (os)
+
+# Dicho comando importa el módulo os y el comando help(os) muestra una descripción de lo que hace el módulo, aparte brindar información detallada de cada función o método sin 
+# tener que recurrir al manual de referencia de internet. Obviamente vía web se puede acceder a https://docs.python.org/es/3/py-modindex.html para este módulo y a otras páginas 
+# para los módulos diseñados por terceros
+
+# Librerías o módulos más utilizados por mi
+
+# Básicos de Python (incorporados directamente en el lenguaje)
+
+# csv: El módulo csv implementa clases para leer y escribir datos tabulares en formato CSV. Permite a los programadores decir, «escribe estos datos en el formato preferido por Excel», 
+# o «lee datos de este archivo que fue generado por Excel», sin conocer los detalles precisos del formato CSV usado por Excel. Los programadores también pueden describir los formatos 
+# CSV entendidos por otras aplicaciones o definir sus propios formatos CSV para fines particulares.
+
+# datetime: El módulo datetime proporciona clases para manipular fechas y horas.
+
+# fileinput: Este módulo implementa una clase auxiliar y funciones para escribir rápidamente un bucle sobre una entrada estándar o una lista de archivos. 
+
+# json: JSON (JavaScript Object Notation), specified by RFC 7159 (which obsoletes RFC 4627) and by ECMA-404, is a lightweight data interchange format inspired by JavaScript object literal 
+# syntax (although it is not a strict subset of JavaScript 1 ).
+# json expone una API familiar a los usuarios de los módulos de la biblioteca estándar marshal y pickle.
+
+# os: Este módulo provee una manera versátil de usar funcionalidades dependientes del sistema operativo.
+
+# math: Este módulo proporciona acceso a las funciones matemáticas definidas en el estándar de C.
+
+# random: Este módulo implementa generadores de números pseudoaleatorios para varias distribuciones.
+
+# subprocess: El módulo subprocess permite lanzar nuevos procesos, conectarse a sus pipes de entrada/salida/error y obtener sus códigos de resultado. Este módulo está destinado 
+# a reemplazar múltiples módulos y funciones previamente existentes: os.system, os.spawn*
+
+# tkinter: El paquete tkinter («interfaz Tk») es la interfaz por defecto de Python para el kit de herramientas de GUI Tk. Desarrollo de aplicaciones de escritorio
+
+
+# Especiales de Python (diseñados por terceros)
+
+# matplotlib
+# numpy
+# pandas
+# pillow
+# pygame
+# scify
+# simple-salesforce
+# tensor
+# wheel
+
