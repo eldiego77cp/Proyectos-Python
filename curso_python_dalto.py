1d051b85c0c27da18c3e619b181cc71a12c66082
eldiego77cp <88398189+eldiego77cp@users.noreply.github.com>
2023-02-18 20:17:34 -0300

    UPD 18022023 20:17

--- curso_python_dalto.py
+++ curso_python_dalto.py
@@ -1,3 +1,4 @@
+# -*- coding: Windows-1252 -*-
 # Ejercicio 1
 
 # Enunciado 
@@ -30,27 +31,41 @@ curso_dalto = 1.5
 curso_dalto_crudo = 3.5
 curso_resto_crudo = 5
 
-# 1a)
-porcentaje_curso_dalto_vs_curso_minimo = (curso_minimo / curso_dalto - 1)*100
-print(f'El curso mínimo es {round(porcentaje_curso_dalto_vs_curso_minimo,2)}% más lento que el de dalto')
-# 1b)
-porcentaje_curso_dalto_vs_curso_maximo = (curso_maximo / curso_dalto - 1)*100
-print(f'El curso máximo es {round(porcentaje_curso_dalto_vs_curso_maximo,2)}% más lento que el de dalto')
-# 1b)
-porcentaje_curso_dalto_vs_curso_promedio = (curso_promedio / curso_dalto - 1)*100
-print(f'El curso promedio es {round(porcentaje_curso_dalto_vs_curso_promedio,2)}% más lento que el de dalto')
+# 1 a-b-c
+porcentaje_curso_dalto_vs_curso_minimo = (1 - curso_dalto / curso_minimo) * 100
+porcentaje_curso_dalto_vs_curso_maximo = (1 - curso_dalto / curso_maximo) * 100
+porcentaje_curso_dalto_vs_curso_promedio = (1 - curso_dalto / curso_promedio) * 100
 
-# 2a)
+print('----------')
+print(f'El curso de dalto es {round(porcentaje_curso_dalto_vs_curso_minimo,2)}% más rápido que el curso de dictado minimo')
+print(f'El curso de dalto es {round(porcentaje_curso_dalto_vs_curso_maximo,2)}% más rápido que el curso de dictado máximo')
+print(f'El curso de dalto es {round(porcentaje_curso_dalto_vs_curso_promedio,2)}% más rápido que el curso de dictado promedio')
+print('----------' + '\n')
+
+# 2 a-b
 porcentaje_reduccion_cursos_general = round((1 - (curso_promedio / curso_resto_crudo))*100,2)
-print(f'El curso promedio se reduce {round(porcentaje_reduccion_cursos_general,2)}% luego de ser editado')
-# 2b)
 porcentaje_reduccion_curso_dalto = round((1 - (curso_dalto / curso_dalto_crudo))*100,2)
+
+
+print(f'El curso promedio se reduce {round(porcentaje_reduccion_cursos_general,2)}% luego de ser editado')
 print(f'El curso de dalto se reduce {round(porcentaje_reduccion_curso_dalto,2)}% luego de ser editado')
+print('----------' + '\n')
 
-# 3a)
+# 3 a-b
 horas_de_otro_curso_equivalentes_curso_dalto = 10 * curso_promedio / curso_dalto
-print(f'Ver 10 horas del curso de dalto equivale a ver {round(horas_de_otro_curso_equivalentes_curso_dalto,2)} hs de otros cursos')
-# 3b)
 horas_curso_dalto_equivalentes_horas_de_otro_curso = 10 * curso_dalto / curso_promedio
-print(f'Ver 10 horas de otros cursos equivale a ver {round(horas_curso_dalto_equivalentes_horas_de_otro_curso,2)} hs del curso de dalto')
 
+print('----------')
+print(f'Ver 10 horas del curso de dalto equivale a ver {round(horas_de_otro_curso_equivalentes_curso_dalto,2)} hs de otros cursos')
+print(f'Ver 10 horas de otros cursos equivale a ver {round(horas_curso_dalto_equivalentes_horas_de_otro_curso,2)} hs del curso de dalto')
+print('----------' + '\n')
+
+# Ejercicio 2
+
+# Enunciado 
+# Suponiendo que cada persona en promedio habla 2 palabras por segundo, pedirle al usuario:
+# a) Escriba un texto real, calcular cuanto tardaría en decir esa frase y contar las palabras que dijo
+# b) Si se tarda más de un minuto decirle "para flaco, tampoco te pedí un testamento"
+# c) Cuanto tardaría dalto si consideramos que es habla un 30% más rápido
+
+frase = input("Ingrese una frase: ")
